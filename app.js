71f7abb75322740c47a2015f91f2b503ae12e9c4
Opeoluwa <88354141+DEVOPEOLUWA@users.noreply.github.com>
2021-10-30 05:21:04 +0100

    Update app.js

--- app.js
+++ app.js
@@ -21,10 +21,3 @@ window.onscroll  = () =>{
 }
 
 
-function loader(){
-  document.querySelector('.loader-container').classList.add('fade-out')
-}
-function fadeOut(){
-  setInterval(loader ,3000)
-}
-window.onload = fadeOut
